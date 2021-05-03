c500b2e7ca5a6cc8f8b179b4de98d11c84514d53
Eleanor <eleanor.ruse@hotmail.co.uk>
2021-05-03 18:53:21 +0100

    Fixed Link Error

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <div className="Footer">
       <small>
-        This project was coded by by <a href="https://www.linkedin.com/in/eleanor-ruse-8aa6a011b/" target="_blank">Eleanor Ruse</a>{" "}and is {" "}
-        <a href="https://github.com/EleanorRuse/react-weather-app" target="_blank">
+        This project was coded by by <a href="https://www.linkedin.com/in/eleanor-ruse-8aa6a011b/">Eleanor Ruse</a>{" "}and is {" "}
+        <a href="https://github.com/EleanorRuse/react-weather-app">
           Open-sourced on Github
         </a>
          </small>
